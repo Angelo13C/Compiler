b57e1088f1ce44a53faae046f07f2080838e1481
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-05-27 10:29:18 +0200

    ✨ Make the main function create an instance of the compiler and run it

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "cli/cli.hpp"
+#include "compiler/compiler.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +10,14 @@ int main(int argc, char* argv[])
     char* pathToFileToCompile = cliArguments.getPathToFileToCompile();
     if(pathToFileToCompile != nullptr)
     {
+        Compiler compiler = Compiler(Tokenizer(), Parser(), Generator(), CompilerSettings {
+            .showTokenizerOutput = true,
+            .showParserOutput = true,
+            .showGeneratorOutput = true
+        });
+        int compileStatus = compiler.compileAndWriteToFile(pathToFileToCompile, "out.asm");
+        if(compileStatus != 0)
+            return compileStatus;
     }
 
     std::cin.get();
