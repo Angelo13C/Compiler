8502f60249178977f6b548218f7879b6406923a4
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-05-27 10:23:22 +0200

    ✨ Add some constants for the NASM code generator

---
+++ src/compiler/generation/special/consts.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+const std::string TAB = "\t";
+const std::string NEW_LINE = "\n";
+const std::string START = "main";
+const std::string STRING_LITERAL_PREFIX = "strLit";
+
+const std::string EXIT_PROCESS_WINDOWS = "ExitProcess";
